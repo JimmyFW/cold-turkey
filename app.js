39ab37d68e8b0b98e8c07a4358aff124f29f05bc
James W <james.wu@alumni.olin.edu>
2016-12-12 23:44:48 -0500

    save reasons to backend

--- app.js
+++ app.js
@@ -1,53 +1,50 @@
 // Reference: http://stackoverflow.com/questions/23339944/remember-state-chrome-extension
-//window.setInterval(blockAndDisplay, 10);
-//document.getElementsByTagName('body')[0].addEventListener("mouseover", checkToBlock);
 
 var blockId = window.setInterval(blockAndDisplay, 10);
 chrome.storage.local.get('toggle', function(data) {
 	if (data.toggle === false) {
-		console.log("no");
+		console.log("Allow content");
 		window.clearInterval(blockId);
 	} else {
-		console.log("yeah");
-		/* do some css inject */
+		console.log("Block content!");
 	}
 });
 
+function submitReason(event) {
+	console.log('submit reason ' + event.keyCode);
+	if (event.keyCode == 13) {
+
+		console.log(this.value);
+		var http = new XMLHttpRequest();
+
+		var queryString = "";
+		queryString += "reason=" + this.value;
+
+		http.open("POST", url, true);
+		http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+		console.log(queryString);
+
+		http.send(queryString);
+	}
+}
+
 function blockAndDisplay() {
 	var content = document.getElementById('content');
-	if (content != null) {
-		content.innerHTML = "Why did you come to YouTube?";
+	if (content.getAttribute('role') != 'weed') {
+		content.innerHTML = "";
+		content.setAttribute('role', 'weed');
+		var textbox = document.createElement('input');
+		textbox.setAttribute('type', 'text');
+		textbox.setAttribute('name', 'reason');
+		textbox.onkeypress = submitReason;
+		var question = document.createTextNode("Why did you come to YouTube?  ");
+		content.appendChild(question);
+		content.appendChild(textbox);
 	}
+
 	var mastHead = document.getElementById('video-masthead-container');
 	if (mastHead != null) {
 		mastHead.innerHTML = "";
 	}
 }
-
-function checkToBlock() {
-	chrome.storage.local.get('toggle', function(data) {
-		if (data.toggle === false) {
-			console.log("no");
-		} else {
-			console.log("yeah");
-			window.setInterval(blockAndDisplay, 10);
-			/* do some css inject */
-		}
-	});
-}
-
-/*
-if (toggle) {
-	document.getElementsByTagName('body')[0].addEventListener("mouseover", blockAndDisplay);
-	window.setInterval(blockAndDisplay, 10);
-}
-*/
-
-
-/*
-var timestamp = new Date().getTime();
-var key = "YouTube" + timestamp;
-chrome.storage.sync.set({key: ""}, function() {
-	console.log("Saved to storage: " + key);
-});
-*/
