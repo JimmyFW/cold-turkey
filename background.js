86eaf63d42ac1da01b5cf96f55a4f40b5b648a6d
James W <james.wu@alumni.olin.edu>
2017-04-09 21:03:26 -0400

    use beginning of day rather than rolling 24 hours

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.requestFreedom) {
 		sendResponse({freedom: localStorage.getItem("freedom")});
+	} else if (request.requestVisits) {
+		chrome.history.getVisits({url:"https://www.youtube.com"}, function(results) {
+			var visitsToday = [];
+			var today = new Date();
+			today.setHours(0, 0, 0, 0);
+
+			for (var i = 0; i < results.length; i++) {
+				if (results[i].visitTime > today.valueOf()) {
+					visitsToday.push(results[i]);
+				}
+			}
+			console.log(visitsToday);
+			sendResponse({visits: visitsToday});
+		});
+		return true;
 	}
 });
